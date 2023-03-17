66fd6fc9ac0b9bd746f545efc24c8edf659a8e3a
Vilma <vilmail@protonmail.com>
2023-03-17 15:04:44 +0200

    andra uzduotis,after long troubleshooting why redirect do not work, had o --ignore json file for nodemon

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { engine } from "express-handlebars";
+import { save, readFileData } from "./save-to-json.js";
 const app = express();
 
 app.engine("handlebars", engine());
@@ -9,17 +10,35 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.render("login");
 });
-app.post("/", (req, res) => {
-  if (req.body.email === "admin@bit.lt" && req.body.password === "1234") {
+app.post("/login", async (req, res) => {
+  let db = await readFileData();
+  db = JSON.parse(db);
+  let ok = false;
+  db.forEach((user) => {
+    if (user.password === req.body.password && user.email === req.body.email) {
+      ok = true;
+    }
+  });
+  if (ok) {
     res.redirect("/administratorius");
   } else {
     res.redirect("/");
   }
+  //   if (req.body.email === "admin@bit.lt" && req.body.password === "1234") {
+  //     res.redirect("/administratorius");
+  //   } else {
+  //     res.redirect("/");
+  //   }
 });
+
 app.get("/administratorius", (req, res) => {
   res.render("admin");
 });
+app.post("/add-user", async (req, res) => {
+  await save(req.body);
+  res.redirect("/");
+});
 
-app.listen(3001, () => {
-  console.log("server on port 3001");
+app.listen(5000, () => {
+  console.log("server on port 5000");
 });
